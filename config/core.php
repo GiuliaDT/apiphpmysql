ad72d180d28d5ace66e12598203ec935200b71b1
GiuliaDT <103998993+GiuliaDT@users.noreply.github.com>
2022-10-20 18:31:27 +0200

    Add files via upload

--- config/core.php
+++ config/core.php
@@ -1,11 +1,9 @@
 <?php
-// show error reporting
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// home page url
-$home_url="http://localhost/api/";
+$home_url="http://localhost/api/apiphpmysql";
 
-// page given in URL parameter, default page is one
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
 
